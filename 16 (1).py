781a5165680e0575d56b4a7ff28aae0a0a738d17
yc <13076091292@163.com>
2016-12-10 19:11:33 +0800

    add py

---
+++ 16 (1).py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 19:35:03 2016
+
+@author: Administrator
+"""
+from fibo import fib1
+print(fib1(10))
---
+++ 16 (2).py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  1 16:09:33 2016
+
+@author: Administrator
+"
+a=2;
+print(a)
